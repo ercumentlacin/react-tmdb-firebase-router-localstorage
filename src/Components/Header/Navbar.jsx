dd5eb627f38164fcb6e4ddf48d75b4a99ce4f63f
ercumenlacin <1140104023@nku.edu.tr>
2021-01-16 18:54:06 +0300

    navbar edit & add css

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,104 +1,83 @@
 /* eslint-disable */
 import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            SPEEDYTV.COM
-          </a>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-          <ul className="p-0 m-0 d-flex align-items-center">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Login
+          <div className="d-flex justify-content-between w-100 flex-wrap align-items-center align-content-center">
+            {/* brand */}
+            <div className="container-brandt">
+              <a className="navbar-brand" href="#">
+                SPEEDYTV.COM
               </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Register
-              </a>
-            </li>
-          </ul>
+            </div>
+            {/* search and button */}
+            <form className="d-flex login-register">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="#"
-                  tabIndex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
-            </ul>
+            {/* login & register */}
+            <div className="d-flex">
+              <ul className="navbar-nav mb-2 mb-lg-0 d-flex flex-row">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Login
+                  </a>
+                </li>
+                <li className="nav-item ms-2">
+                  <a className="nav-link" href="#">
+                    Register
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            {/* toggle */}
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            {/* toggle menu ıtems */}
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mb-2 mb-lg-0 d-flex flex-column">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    About Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Blog
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contack Me
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
