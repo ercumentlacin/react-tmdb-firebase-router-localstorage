120c889ecbfa6aa6869b9e04494d24d96ebea22f
ercumenlacin <1140104023@nku.edu.tr>
2021-01-16 20:28:22 +0300

    edit sectionhero component

--- src/Components/SectionHero/SectionHero.jsx
+++ src/Components/SectionHero/SectionHero.jsx
@@ -7,16 +7,29 @@ export default function SectionHero() {
   const constrat = theme === "light" ? "dark" : "light";
   return (
     <div>
-      <div className={`container bg-${theme} text-${constrat} `}>
-        <div className="row">
+      <div className={`container bg-${theme} text-${constrat} pt-lg-5`}>
+        <div className="row align-items-center">
           <div className="col-md-6">
-            <h1>
+            <h1 className="display-2">
               <span className="text-warning">THE FIRST</span> ONLINE <br />
               STREAMING MOVIE <br />
               SEARCH ENGINE
             </h1>
           </div>
-          <div className="col-md-6">deneme</div>
+          <div className="col-md-6">
+            {" "}
+            <p>
+              Speedy TV is an online streaming movie search engine with filters
+              to find best movies for you. Use our website to watch movies
+              online throught the Netflix catalog. İts easy..
+            </p>{" "}
+            <p>
+              Sort and filter streaming movies based on release year , IMDB and
+              Rotten Tomatoes ratings and Age/MPAA ratings. The click on the
+              movie to get more details and a direct link to view the movie
+              online.
+            </p>
+          </div>
         </div>
       </div>
     </div>
